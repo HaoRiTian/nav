ee34214926e6902924724cc9f086cb572a5bef0c
HaoRiTian <48886677+HaoRiTian@users.noreply.github.com>
2021-01-29 10:39:56 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -12,10 +12,10 @@ const c: IConfig = {
   homeUrl: 'https://nav3.cn',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '导航 - 精选实用导航网站',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '精选实用导航网站',
 
   // 网站关键字
   keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
